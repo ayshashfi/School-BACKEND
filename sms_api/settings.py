857d9077f9f08c278a87834806e09f2742db974d
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-09-13 22:24:12 +0530

    ip

--- sms_api/settings.py
+++ sms_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yv#&8*=-$##_vtgsbf65wj5ayq(2w=7^*+28!0y+lfsl0#bk62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '13.48.27.195']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '16.170.218.34']
 
 
 # Application definition
