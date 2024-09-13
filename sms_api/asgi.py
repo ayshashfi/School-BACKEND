c8b7c36c9da6abe529d775352f9ab382ea22942f
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-09-13 23:40:57 +0530

    hosting

--- sms_api/asgi.py
+++ sms_api/asgi.py
@@ -5,14 +5,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
-from chat.channels_middleware import JWTwebsocketMiddleware
-
 # Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_api.settings')
 
-# Initialize Django
-django.setup()
+# Initialize Django application
+django.setup()  # Call setup before importing anything else Django-related
+
+# Now import other necessary modules
+from chat.routing import websocket_urlpatterns
+from chat.channels_middleware import JWTwebsocketMiddleware
 
 # Initialize Django ASGI application
 django_asgi_app = get_asgi_application()
@@ -28,3 +29,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
